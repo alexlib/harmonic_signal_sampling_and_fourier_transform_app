e479c4b96ad3932f91f78690a2858300e303f7a5
Alex Liberzon <alexlib@users.noreply.github.com>
2022-11-19 09:42:17 +0200

    working version with sliders on the sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,11 @@ import streamlit as st
 from pathlib import Path
 import base64
 
+from scipy import fft
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 # Initial page config
 
 st.set_page_config(
@@ -30,6 +35,27 @@ st.set_page_config(
      layout="wide",
      initial_sidebar_state="expanded",
 )
+
+def spectrum(y, Fs):
+    """
+    Plots a Single-Sided Amplitude Spectrum of a sampled
+    signal y(t), sampling frequency Fs (length of a signal
+    provides the number of samples recorded)
+
+    Following: http://goo.gl/wRoUn
+    """
+    n = len(y)  # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T  # two sides frequency range
+    frq = frq[range(np.int(n/2))]  # one side frequency range
+    Y = 2*fft.fft(y)/n  # fft computing and normalization
+    Y = Y[range(np.int(n/2))]
+    return (frq, Y)
+
+
+
+
 
 def main():
     cs_sidebar()
@@ -48,45 +74,54 @@ def img_to_bytes(img_path):
 
 def cs_sidebar():
 
-    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
-    st.sidebar.header('Streamlit cheat sheet')
 
-    st.sidebar.markdown('''
-<small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v1.8.0](https://www.streamlit.io/).</small>
-    ''', unsafe_allow_html=True)
+    # N = st.sidebar.checkbox('Streamlines?',value = False)
 
-    st.sidebar.markdown('__How to install and import__')
+    # colorbar = st.sidebar.checkbox('Color bar?',value=False)
 
-    st.sidebar.code('$ pip install streamlit')
+    # average = st.sidebar.checkbox('Average?',value=False)
 
-    st.sidebar.markdown('Import convention')
-    st.sidebar.code('>>> import streamlit as st')
 
-    st.sidebar.markdown('__Add widgets to sidebar__')
-    st.sidebar.code('''
-st.sidebar.<widget>
->>> a = st.sidebar.radio(\'R:\',[1,2])
-    ''')
 
-    st.sidebar.markdown('__Command line__')
-    st.sidebar.code('''
-$ streamlit --help
-$ streamlit run your_script.py
-$ streamlit hello
-$ streamlit config show
-$ streamlit cache clear
-$ streamlit docs
-$ streamlit --version
-    ''')
+#     st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
+#     st.sidebar.header('Streamlit cheat sheet')
 
-    st.sidebar.markdown('__Pre-release features__')
-    st.sidebar.markdown('[Beta and experimental features](https://docs.streamlit.io/library/advanced-features/prerelease#beta-and-experimental-features)')
-    st.sidebar.code('''
-pip uninstall streamlit
-pip install streamlit-nightly --upgrade
-    ''')
+#     st.sidebar.markdown('''
+# <small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v1.8.0](https://www.streamlit.io/).</small>
+#     ''', unsafe_allow_html=True)
 
-    st.sidebar.markdown('''<small>[st.cheat_sheet v1.8.0](https://github.com/daniellewisDL/streamlit-cheat-sheet)  | April 2022</small>''', unsafe_allow_html=True)
+#     st.sidebar.markdown('__How to install and import__')
+
+#     st.sidebar.code('$ pip install streamlit')
+
+#     st.sidebar.markdown('Import convention')
+#     st.sidebar.code('>>> import streamlit as st')
+
+#     st.sidebar.markdown('__Add widgets to sidebar__')
+#     st.sidebar.code('''
+# st.sidebar.<widget>
+# >>> a = st.sidebar.radio(\'R:\',[1,2])
+#     ''')
+
+#     st.sidebar.markdown('__Command line__')
+#     st.sidebar.code('''
+# $ streamlit --help
+# $ streamlit run your_script.py
+# $ streamlit hello
+# $ streamlit config show
+# $ streamlit cache clear
+# $ streamlit docs
+# $ streamlit --version
+#     ''')
+
+#     st.sidebar.markdown('__Pre-release features__')
+#     st.sidebar.markdown('[Beta and experimental features](https://docs.streamlit.io/library/advanced-features/prerelease#beta-and-experimental-features)')
+#     st.sidebar.code('''
+# pip uninstall streamlit
+# pip install streamlit-nightly --upgrade
+#     ''')
+
+#     st.sidebar.markdown('''<small>[st.cheat_sheet v1.8.0](https://github.com/daniellewisDL/streamlit-cheat-sheet)  | April 2022</small>''', unsafe_allow_html=True)
 
     return None
 
@@ -95,259 +130,36 @@ pip install streamlit-nightly --upgrade
 ##########################
 
 def cs_body():
-    # Magic commands
     st.title("Harmonic signal sampling and Fourier transform app")
-    col1, col2, col3 = st.columns(3)
 
-    col1.subheader('Magic commands')
-    col1.code('''# Magic commands implicitly `st.write()`
-\'\'\' _This_ is some __Markdown__ \'\'\'
-a=3
-'dataframe:', data
-    ''')
+    st.subheader('Sample signal and its spectrum')
 
-    # Display text
+    N = st.sidebar.slider('Number of samples, N', min_value=64, max_value=1024, step=64)
+    ff = st.sidebar.slider('Signal frequency (Hz) ',  min_value=1, max_value=15)
+    fs = st.sidebar.slider('Sampling frequency (Hz)',min_value=5, max_value=150)
+    A = st.sidebar.slider('Amplitude [V]',min_value=1, max_value=10)
 
-    col1.subheader('Display text')
-    col1.code('''
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see *
-st.caption('Balloons. Hundreds of them...')
-st.latex(r\'\'\' e^{i\pi} + 1 = 0 \'\'\')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
+    T = N/fs  # sampling period
+    t = np.arange(0.0, T, T/N)  # sampling time steps
+    y = A*np.sin(2*np.pi*ff*t)  # sampled signal
+    frq, Y = spectrum(y, fs)  # FFT(sampled signal)
 
-* optional kwarg unsafe_allow_html = True
+    # Plot
+    fig, ax = plt.subplots(2, 1, figsize=(10, 8))
+    ax[0].plot(t, y, 'b.')
+    ax[0].set_xlabel('$t$ [s]')
+    ax[0].set_ylabel('Y [V]')
+    ax[0].plot(t[0], y[0], 'ro')
+    ax[0].plot(t[-1], y[-1], 'ro')
 
-    ''')
+    ax[1].plot(frq, abs(Y), 'r')  # plotting the spectrum
+    ax[1].set_xlabel('$f$ (Hz)')
+    ax[1].set_ylabel('$|Y(f)|$')
+    lgnd = str(f'N = {N}, f = {fs} Hz')
+    ax[1].legend([lgnd], loc='best')
 
-    # Display data
+    st.pyplot(fig)
 
-    col1.subheader('Display data')
-    col1.code('''
-st.dataframe(my_dataframe)
-st.table(data.iloc[0:10])
-st.json({'foo':'bar','fu':'ba'})
-st.metric(label="Temp", value="273 K", delta="1.2 K")
-    ''')
-
-    # Display charts
-
-    col1.subheader('Display charts')
-    col1.code('''
-st.line_chart(data)
-st.area_chart(data)
-st.bar_chart(data)
-st.pyplot(fig)
-st.altair_chart(data)
-st.vega_lite_chart(data)
-st.plotly_chart(data)
-st.bokeh_chart(data)
-st.pydeck_chart(data)
-st.deck_gl_chart(data)
-st.graphviz_chart(data)
-st.map(data)
-    ''')
-
-    # Display media
-
-    col1.subheader('Display media')
-    col1.code('''
-st.image('./header.png')
-st.audio(data)
-st.video(data)
-    ''')
-
-    # Display interactive widgets
-
-    col1.subheader('Display interactive widgets')
-    col1.code('''
-st.button('Hit me')
-st.download_button('On the dl', data)
-st.checkbox('Check me out')
-st.radio('Radio', [1,2,3])
-st.selectbox('Select', [1,2,3])
-st.multiselect('Multiselect', [1,2,3])
-st.slider('Slide me', min_value=0, max_value=10)
-st.select_slider('Slide to select', options=[1,'2'])
-st.text_input('Enter some text')
-st.number_input('Enter a number')
-st.text_area('Area for textual entry')
-st.date_input('Date input')
-st.time_input('Time entry')
-st.file_uploader('File uploader')
-st.camera_input("一二三,茄子!")
-st.color_picker('Pick a color')
-    ''')
-    col1.write('Use widgets\' returned values in variables:')
-    col1.code('''
->>> for i in range(int(st.number_input('Num:'))): foo()
->>> if st.sidebar.selectbox('I:',['f']) == 'f': b()
->>> my_slider_val = st.slider('Quinn Mallory', 1, 88)
->>> st.write(slider_val)
-    ''')
-
-    # Control flow
-
-    col2.subheader('Control flow')
-    col2.code('''
-st.stop()
-    ''')
-
-    # Lay out your app
-
-    col2.subheader('Lay out your app')
-    col2.code('''
-st.form('my_form_identifier')
-st.form_submit_button('Submit to me')
-st.container()
-st.columns(spec)
->>> col1, col2 = st.columns(2)
->>> col1.subheader('Columnisation')
-st.expander('Expander')
->>> with st.expander('Expand'):
->>>     st.write('Juicy deets')
-    ''')
-
-    col2.write('Batch widgets together in a form:')
-    col2.code('''
->>> with st.form(key='my_form'):
->>> 	text_input = st.text_input(label='Enter some text')
->>> 	submit_button = st.form_submit_button(label='Submit')
-    ''')
-
-    # Display code
-
-    col2.subheader('Display code')
-    col2.code('''
-st.echo()
->>> with st.echo():
->>>     st.write('Code will be executed and printed')
-    ''')
-
-    # Display progress and status
-
-    col2.subheader('Display progress and status')
-    col2.code('''
->>> with st.spinner(text='In progress'):
->>>   time.sleep(5)
->>>   st.success('Done')
-
-st.progress(progress_variable_1_to_100)
-st.balloons()
-st.snow()
-st.error('Error message')
-st.warning('Warning message')
-st.info('Info message')
-st.success('Success message')
-st.exception(e)
-    ''')
-
-    # Placeholders, help, and options
-
-    col2.subheader('Placeholders, help, and options')
-    col2.code('''
-# Replace any single element.
->>> element = st.empty()
->>> element.line_chart(...)
->>> element.text_input(...)  # Replaces previous.
-
-# Insert out of order.
->>> elements = st.container()
->>> elements.line_chart(...)
->>> st.write("Hello")
->>> elements.text_input(...)  # Appears above "Hello".
-
-st.help(pandas.DataFrame)
-st.get_option(key)
-st.set_option(key, value)
-st.set_page_config(layout='wide')
-st.experimental_show(objects)
-st.experimental_get_query_params()
-st.experimental_set_query_params(**params)
-    ''')
-
-    # Mutate data
-
-    col3.subheader('Mutate data')
-    col3.code('''
-# Add rows to a dataframe after
-# showing it.
->>> element = st.dataframe(df1)
->>> element.add_rows(df2)
-
-# Add rows to a chart after
-# showing it.
->>> element = st.line_chart(df1)
->>> element.add_rows(df2)
-    ''')
-
-    # Optimize performance
-
-    col3.subheader('Optimize performance')
-    col3.write('Legacy caching')
-    col3.code('''
->>> @st.cache
-... def foo(bar):
-...   # Do something expensive in here...
-...   return data
->>> # Executes foo
->>> d1 = foo(ref1)
->>> # Does not execute foo
->>> # Returns cached item by reference, d1 == d2
->>> d2 = foo(ref1)
->>> # Different arg, so function foo executes
->>> d3 = foo(ref2)
-    ''')
-    col3.write('Cache data objects')
-    col3.code('''
->>> @st.experimental_memo
-... def foo(bar):
-...   # Do something expensive and return data
-...   return data
-# Executes foo
->>> d1 = foo(ref1)
-# Does not execute foo
-# Returns cached item by value, d1 == d2
->>> d2 = foo(ref1)
-# Different arg, so function foo executes
->>> d3 = foo(ref2)
-# Clear all cached entries for this function
->>> foo.clear()
-# Clear values from *all* memoized functions
->>> st.experimental_memo.clear()
-    ''')
-    col3.write('Cache non-data objects')
-    col3.code('''
-# E.g. TensorFlow session, database connection, etc.
->>> @st.experimental_singleton
-... def foo(bar):
-...   # Create and return a non-data object
-...   return session
-# Executes foo
->>> s1 = foo(ref1)
-# Does not execute foo
-# Returns cached item by reference, d1 == d2
->>> s2 = foo(ref1)
-# Different arg, so function foo executes
->>> s3 = foo(ref2)
-# Clear all cached entries for this function
->>> foo.clear()
-# Clear all singleton caches
->>> st.experimental_singleton.clear()
-    ''')
-
-    col3.subheader('Other key parts of the API')
-    col3.markdown('''
-<small>[State API](https://docs.streamlit.io/en/stable/session_state_api.html)</small><br>
-<small>[Theme option reference](https://docs.streamlit.io/en/stable/theme_options.html)</small><br>
-<small>[Components API reference](https://docs.streamlit.io/en/stable/develop_streamlit_components.html)</small><br>
-<small>[API cheat sheet](https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py)</small><br>
-''', unsafe_allow_html=True)
 
     return None
 
