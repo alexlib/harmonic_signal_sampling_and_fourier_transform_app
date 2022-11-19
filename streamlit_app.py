135a736bfd77e5a8d7cbd5765ea822e14cd4173d
Alex Liberzon <alexlib@users.noreply.github.com>
2022-11-19 10:41:30 +0200

    updated to use only numpy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,8 @@
-"""
-Streamlit Cheat Sheet
-
-App to summarise streamlit docs v1.8.0
-
-There is also an accompanying png and pdf version
-
-https://github.com/daniellewisDL/streamlit-cheat-sheet
-
-v1.8.0 October 2021
-
-Author:
-    @daniellewisDL : https://github.com/daniellewisDL
-
-Contributors:
-    @arnaudmiribel : https://github.com/arnaudmiribel
-    @akrolsmir : https://github.com/akrolsmir
-    @nathancarter : https://github.com/nathancarter
-
-"""
-
+""" Streamlit app """
 import streamlit as st
 from pathlib import Path
 import base64
 
-from scipy import fft
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -49,8 +28,8 @@ def spectrum(y, Fs):
     T = n/Fs
     frq = k/T  # two sides frequency range
     frq = frq[range(np.int(n/2))]  # one side frequency range
-    Y = 2*fft.fft(y)/n  # fft computing and normalization
-    Y = Y[range(np.int(n/2))]
+    Y = 2*np.fft.fft(y)/n  # fft computing and normalization
+    Y = Y[range(int(n/2))]
     return (frq, Y)
 
 
@@ -58,7 +37,7 @@ def spectrum(y, Fs):
 
 
 def main():
-    cs_sidebar()
+    # cs_sidebar()
     cs_body()
 
     return None
@@ -69,65 +48,6 @@ def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
     return encoded
-
-# sidebar
-
-def cs_sidebar():
-
-
-    # N = st.sidebar.checkbox('Streamlines?',value = False)
-
-    # colorbar = st.sidebar.checkbox('Color bar?',value=False)
-
-    # average = st.sidebar.checkbox('Average?',value=False)
-
-
-
-#     st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
-#     st.sidebar.header('Streamlit cheat sheet')
-
-#     st.sidebar.markdown('''
-# <small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v1.8.0](https://www.streamlit.io/).</small>
-#     ''', unsafe_allow_html=True)
-
-#     st.sidebar.markdown('__How to install and import__')
-
-#     st.sidebar.code('$ pip install streamlit')
-
-#     st.sidebar.markdown('Import convention')
-#     st.sidebar.code('>>> import streamlit as st')
-
-#     st.sidebar.markdown('__Add widgets to sidebar__')
-#     st.sidebar.code('''
-# st.sidebar.<widget>
-# >>> a = st.sidebar.radio(\'R:\',[1,2])
-#     ''')
-
-#     st.sidebar.markdown('__Command line__')
-#     st.sidebar.code('''
-# $ streamlit --help
-# $ streamlit run your_script.py
-# $ streamlit hello
-# $ streamlit config show
-# $ streamlit cache clear
-# $ streamlit docs
-# $ streamlit --version
-#     ''')
-
-#     st.sidebar.markdown('__Pre-release features__')
-#     st.sidebar.markdown('[Beta and experimental features](https://docs.streamlit.io/library/advanced-features/prerelease#beta-and-experimental-features)')
-#     st.sidebar.code('''
-# pip uninstall streamlit
-# pip install streamlit-nightly --upgrade
-#     ''')
-
-#     st.sidebar.markdown('''<small>[st.cheat_sheet v1.8.0](https://github.com/daniellewisDL/streamlit-cheat-sheet)  | April 2022</small>''', unsafe_allow_html=True)
-
-    return None
-
-##########################
-# Main body of cheat sheet
-##########################
 
 def cs_body():
     st.title("Harmonic signal sampling and Fourier transform app")
